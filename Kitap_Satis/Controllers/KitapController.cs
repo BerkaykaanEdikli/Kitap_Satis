c4ee9c4a053a42908028b7bebf64f01d24150761
berkay769 <berkay769@BPC03>
2024-02-23 10:04:12 +0300

    Proje dosyası ekle.

---
+++ Kitap_Satis/Controllers/KitapController.cs
@@ -0,0 +1,20 @@
+﻿using Kitap_Satis.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kitap_Satis.Controllers
+{
+    public class KitapController : Controller
+    {
+        public IActionResult Index()
+        {
+            KitapModel kitap = new KitapModel();
+            kitap.KitapId = 001;
+            kitap.KitapAdi = "Hacı Murat";
+            kitap.SayfaSayisi = 164;
+            kitap.YazarAdi = "Fyodor Mihayloviç Dostayevski";
+            kitap.KitapTuru = "Roman";
+            ViewBag.Kitap = kitap;
+            return View();
+        }
+    }
+}
---
+++ Kitap_Satis/Models/KitapModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Kitap_Satis.Models
+{
+    public class KitapModel
+    {
+        public int KitapId { get; set; }
+        public string KitapAdi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string YazarAdi { get; set; }
+        public string KitapTuru { get; set; }
+    }
+}
